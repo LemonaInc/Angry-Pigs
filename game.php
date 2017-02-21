b11879bb7f5070826de59511d4784a2b187e5e8c
LemonaInc <jaxonstevens@lemonainc.com>
2017-02-20 18:20:23 -0800

    Removed game.php editing elements in sidebar

--- game.php
+++ game.php
@@ -28,7 +28,7 @@
 	<div class="container">
 		<!-- Set Background of Level editor -->
 		<a id="editButton" href="#" data-activates="slide-out"
-			class=" button-collapse btn-large waves-effect waves-light #00bcd4 cyan">Edit Level</a>
+			class=" button-collapse btn-large waves-effect waves-light #ffd54f amber lighten-2">Game Menu</a>
 		<div id="gameScreen">
 			<!--  Filled in with actual level info programmatically -->
 			<div id="title"> <!-- title will be cleared on menu selection -->
@@ -49,90 +49,13 @@
 
 		<!-- Slide out Navbar -->
 		<ul id="slide-out" class="side-nav">
-			<li><a href="javscript:void(0)"
 
-
-				class="tabLinks  btn-large waves-effect waves-light #00bcd4 cyan"
-				onclick="openTab(event, 'levelEditor')">Level</a></li>
-			<li><a href="javscript:void(0)"
-
-				class="tabLinks  btn-large waves-effect waves-light #00bcd4 cyan"
-				onclick="openTab(event, 'objectList')">Objects</a></li>
-			<li><a href="javscript:void(0)"
-
-				class="tabLinks  btn-large waves-effect waves-light #00bcd4 cyan"
-				onclick="openTab(event, 'objectProperties')">Properties</a></li>
 			<li><div class="userView">
 					<div class="background">
 						<img src="images/longbackground.png">
 					</div>
 
-					<div id="objectProperties" class="tabContent">
-						<form class="objectPropertiesForm" accept-charset="utf-8">
-							<h4>Position</h4>
-							 <input type="text" name="xPos" value="" class="#424242 grey darken-3"
-								placeholder="X Position" /><br>  <input type="text"
-								name="yPos" value="" class="#424242 grey darken-3" placeholder="Y Position" />
-							<h4>Size</h4>
-							 <input type="text" name="width" value="" class="#424242 grey darken-3"
-								placeholder="Width" /><br>  <input type="text"
-								name="height" value="" class="#424242 grey darken-3" placeholder="Height" />
-							<h5>Physics Properties</h5>
-							 <input type="text" name="bounce" value="" class="#424242 grey darken-3"
-								placeholder="Bounce" /><br>  <input type="text"
-								name="mass" value="" class="#424242 grey darken-3" placeholder="Mass" /><br> <input
-								type="text" name="friction" class="waves-light #424242 grey darken-3" value="" placeholder="Friction" />
-							<h4>Texture</h4>
-							texture: <input type="text" name="texture" value="" class="#424242 grey darken-3"
-								placeholder="URL of texture" /> <input type="submit"
-								class="btn-large waves-effect waves-light #424242 grey darken-3"
-								name="action" value="submit" />
-						</form>
-					</div>
 
-					<div id="levelEditor" class="tabContent">
-						<form class="saveLevelForm" accept-charset="utf-8">
-							<h5>Save</h5>
-							Level: <input type="text" name="levelName" value="" class="#424242 grey darken-3"
-								placeholder="Level Name" /> Author: <input type="text"
-								name="author" value="" class="#424242 grey darken-3" placeholder="Level Name" />
-
-							<h4>Properties</h4>
-							Projectiles: <input type="text" name="projectiles" value="" class="#424242 grey darken-3"
-								placeholder="Amount of projectiles" /> Background Image: <input
-								type="text" name="bgImage" value="../images/Field.jpg" class="#424242 grey darken-3" /> <input
-								type="submit"
-								class="btn-large waves-effect waves-light #424242 grey darken-3"
-								name="action" value="save" />
-						</form>
-
-						<!-- End of this form -->
-
-						<form class="loadLevelForm" accept-charset="utf-8">
-							<h5>Load a level</h5>
-						 <select id="levelSelectDropdown"
-								class='dropdown-button btn' name="Level">
-								<!--  generated in code -->
-							</select> <input type="submit" name="action"
-								class="btn-large waves-effect waves-light#424242 grey darken-3"
-								value="load" />
-						</form>
-
-					</div>
-
-					<div id="objectList" class="tabContent">
-						<div id="wall" class="objectInList"></div>
-						<div id="captive" class="objectInList"></div>
-						<div id="Tree" class="objectInList"></div>
-            <div id="wallBlock" class="objectInList"></div>
-						<div id="redBlock" class="objectInList"></div>
-						<div id="cube" class="objectInList"></div>
-						<div id="bananas" class="objectInList"></div>
-
-
-					</div>
-
-				</div>
 		</ul>
 	</div>
 
